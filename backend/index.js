9866ebbd63e0d5acebcd42b06090027cdf33c49b
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-08-06 11:18:44 +0530

    linka

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,8 @@ const path = require('path');
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
+  origin: process.env.FRONTEND_URL,
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());

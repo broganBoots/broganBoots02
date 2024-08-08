2631e2625d759671fa2c267f6bd4f6d19dcb0c10
Pareshrout5678 <162680503+Pareshrout5678@users.noreply.github.com>
2024-08-08 12:01:07 +0530

    Update index.js
    fixed cross issue

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(cors({
-  origin: ['https://inspiring-paletas-d7daba.netlify.app/', 'http://localhost:5173/'],
+  origin: "https://inspiring-paletas-d7daba.netlify.app/",
   credentials: true
 }));
 app.use(express.json());
